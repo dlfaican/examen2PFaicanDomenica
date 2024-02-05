1396db88411eedd1d8455268c2a676cc7e133e3d
dlfaican <dlfaican@espe.edu.ec>
2024-02-05 18:31:42 -0500

    estructura

---
+++ src/main/java/ec/edu/espe/segundoparcial/examenfaican/ExamenfaicanApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.segundoparcial.examenfaican;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenfaicanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenfaicanApplication.class, args);
+	}
+
+}
